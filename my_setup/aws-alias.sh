9fd1319daf9361be76d603590b6280049589c482
Lan Huong Nguyen <nlhuong90@gmail.com>
2017-11-20 23:13:25 -0800

    Modify setup files

--- my_setup/aws-alias.sh
+++ my_setup/aws-alias.sh
@@ -2,6 +2,26 @@ alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=
 alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
 alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
 alias aws-ip='export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
-alias aws-ssh-p2='ssh -i ~/.ssh/aws/aws-key-fast-ai.pem -L 443:127.0.0.1:8888 ubuntu@$instanceIp'
-alias aws-ssh-t2='ssh -i ~/.ssh/aws/demo.pem -L 443:127.0.0.1:8888 ec2-user@$instanceIp'
+alias aws-ssh-p2='ssh -i ~/.ssh/aws/aws-key-fast-ai.pem ubuntu@$instanceIp'
+alias aws-ssh-t2='ssh -i ~/.ssh/aws/demo.pem ec2-user@$instanceIp'
 alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
+alias aws-state='aws ec2 describe-instances --instance-ids $instanceId --query "Reservations[0].Instances[0].State.Name"'
+
+
+if [[ `uname` == *"CYGWIN"* ]]
+then
+    # This is cygwin.  Use cygstart to open the notebook
+    alias aws-nb='cygstart http://$instanceIp:8888'
+fi
+
+if [[ `uname` == *"Linux"* ]]
+then
+    # This is linux.  Use xdg-open to open the notebook
+    alias aws-nb='xdg-open http://$instanceIp:8888'
+fi
+
+if [[ `uname` == *"Darwin"* ]]
+then
+    # This is Mac.  Use open to open the notebook
+    alias aws-nb='open http://$instanceIp:8888'
+fi
